1b4d5f69b46e8d24a1d713ab23ed8dbd0559c38a
Mahidhar Mannuru <81514511+MahidharMannuru5@users.noreply.github.com>
2021-12-15 05:55:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express  = require('express');
 const app = express();
 const Quotes=require("./quotes.json");
-app.get('/',(req, res) => {
-    res.send("hello world");
+
 
 app.get('/Quotes',(req,res)=>{
 
     res.send(Quotes);
 });
 
-});
+
 const port =process.env.PORT || 3000;
 app.listen(port,() => console.log('listening on port 3000'));
