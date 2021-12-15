81a983619382d60a179bfb4c80491ef2ae64a21c
Mahidhar Mannuru <81514511+MahidharMannuru5@users.noreply.github.com>
2021-12-15 05:50:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express  = require('express');
+const app = express();
+const Quotes=require("./quotes.json");
+app.get('/',(req, res) => {
+    res.send("hello world");
+
+app.get('/Quotes',(req,res)=>{
+
+    res.send(Quotes);
+});
+
+});
+const port =process.env.PORT || 3000;
+app.listen(port,() => console.log('listening on port 3000'));
